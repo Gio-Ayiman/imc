bbd508c1a76629750be545d50abf8400198945b9
Giovanni Ange Ivane Ayimambenwe <gioayima@gmail.com>
2021-12-21 10:02:50 +0100

    modification du css

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
-<?php?>
 <html lang="fr">
   <head>
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="./css/style.css">
+    <link rel="shortcut icon" href="./img/ico/favicon.ico" type="image/x-icon">
 
     <title>Mon IMC</title>
   </head>
--- php/liste.php
+++ php/liste.php
@@ -4,22 +4,22 @@
     
     <?php require 'connex.php';
 
-    $stmt = $conn->prepare("SELECT annee, nom, indice, statut FROM liste");
-    $stmt->execute();
-    $result = $stmt->fetchAll();
+        $stmt = $conn->prepare("SELECT annee, nom, indice, statut FROM liste");
+        $stmt->execute();
+        $result = $stmt->fetchAll();
 
-    echo "<div id='div-table'><table id='myTable'><thead><tr><th>Annee</th><th>Nom</th><th>Indice</th><th>Statut</th></tr></thead>";
+        echo "<div id='div-table'><table id='myTable'><thead><tr><th>Annee</th><th>Nom</th><th>Indice</th><th>Statut</th></tr></thead>";
 
-    foreach($result as $res) {
-       echo "<tbody><td>".$res['annee'] . "</td>"."<td>". $res['nom'] . "</td>" . "<td>" . $res['indice'] . "</td>" . "<td>". $res['statut']."</td>";
-    };
+        foreach($result as $res) {
+        echo "<tbody><td>".$res['annee'] . "</td>"."<td>". $res['nom'] . "</td>" . "<td>" . $res['indice'] . "</td>" . "<td>". $res['statut']."</td>";
+        };
 
-    echo "</table></div>";
+        echo "</table></div>";
 
-    $conn = null;
-?>
-    <div id="button-back" class="button"><a href="/imc/index.php" id="" >Retour à l'acceuil</a></div>
-</body>        
+        $conn = null;
+    ?>
+    <div id="div-button-back"><a href="../index.php" id="link-back">Retour à l'acceuil</a></div>
+    </body>        
 </html>
 
     
